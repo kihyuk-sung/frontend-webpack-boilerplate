19ac10f1c91fb6775cf6ab54a6ee41cbdfc46e79
K <kihyuk.sung@outlook.kr>
2021-09-08 21:04:57 +0900

    feat: font 관련 설정 추가

--- webpack.common.js
+++ webpack.common.js
@@ -21,6 +21,13 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]',
+        },
+      },
     ],
   },
   plugins: [new HtmlWebpackPlugin({ title }), new MiniCssExtractPlugin()],
